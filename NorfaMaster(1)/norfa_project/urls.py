113ba98a06e6b50300580350d17e591b74e09784
DudinOleg <86569646+DudinOleg@users.noreply.github.com>
2022-06-18 23:22:01 +0300

    Add files via upload

---
+++ NorfaMaster(1)/norfa_project/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include 
+from django.views.generic.base import TemplateView 
+from users.views import ClientSignUpView,CompanySignUpView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('users/', include('users.urls')), 
+    path('users/', include('django.contrib.auth.urls')), 
+    path('', TemplateView.as_view(template_name='home.html'), name='home'), 
+    path('client_signup/', ClientSignUpView.as_view(), name='signup'),
+    path('company_signup/', CompanySignUpView.as_view(), name='company_signup'),
+    path('services/', include('services.urls')), 
+]
---
+++ NorfaMaster(1)/services/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from .views import ServicesListView
+
+urlpatterns = [
+    path('', ServicesListView.as_view(), name='services_list'),
+]
